6279271986cc3da6892090e4a7c7dbbafd6b1045
phogtr <phogbtr@gmail.com>
2021-10-06 21:52:31 -0700

    custom Hook modify localStorage

--- src/react-mf-utils.ts
+++ src/react-mf-utils.ts
@@ -1,10 +1,21 @@
 // Anything exported from this file is importable by other in-browser modules.
-export function publicApiFunction() {
-  return "public api function";
-}
-
-export const helloWorldFc = () => {
-  return {
-    echo: "Hello World Fc utils",
+export const useLocalStorage = () => {
+  const getData = () => {
+    const data = localStorage.getItem("data");
+    const parsedData = JSON.parse(data || "{}");
+    return parsedData;
   };
+
+  const saveData = (newData: any) => {
+    localStorage.setItem("data", JSON.stringify(newData));
+  };
+
+  return {
+    localStorageData: getData(),
+    setLocalStorageData: saveData,
+  };
+};
+
+export const clearLocalStorage = () => {
+  localStorage.removeItem("data");
 };
